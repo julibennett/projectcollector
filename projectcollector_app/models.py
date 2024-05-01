4b3f109e5cc9e0d5c9ebb3f00cfe75ca2e4b0bee
julibennett <julibennett4@gmail.com>
2024-05-01 14:13:55 -0700

    part 2, still working a little on details page

--- projectcollector_app/models.py
+++ projectcollector_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    technologies = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    live = models.CharField(max_length=150)
+    created = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- projectcollector_app/urls.py
+++ projectcollector_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('projects/', views.projects_index, name='index'),
+    path('projects/<int:project_id>/', views.projects_detail, name='detail'),
 ]
 
